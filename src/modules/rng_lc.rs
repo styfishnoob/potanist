803581bfeeb2c01297418a76b98d6eb4c2335287
styfishnoob <main.sakana@gmail.com>
2025-05-09 18:55:07 +0900

    refactor

--- src/modules/rng_lc.rs
+++ src/modules/rng_lc.rs
@@ -1,10 +1,12 @@
-pub struct LCRng {
+use crate::types::types::*;
+
+pub struct RngLC {
     multiplier: u32,
     increment: u32,
     inverse_multiplier: u32,
 }
 
-impl LCRng {
+impl RngLC {
     pub fn new() -> Self {
         Self {
             multiplier: 0x41c64e6d,
@@ -13,17 +15,18 @@ impl LCRng {
         }
     }
 
-    pub fn next(&self, seed: u32) -> u32 {
+    pub fn next(&self, seed: Seed) -> Seed {
         let next_seed = (seed as u64) * (self.multiplier as u64) + (self.increment as u64);
-        return (next_seed & 0xffffffff) as u32;
+        return (next_seed & 0xffffffff) as Seed;
     }
 
-    pub fn prev(&self, seed: u32) -> u32 {
-        let prev_seed = (self.inverse_multiplier as u64) * (seed as u64 - self.increment as u64);
-        return (prev_seed & 0xffffffff) as u32;
+    pub fn prev(&self, seed: Seed) -> Seed {
+        let diff = seed.wrapping_sub(self.increment);
+        let prev_seed = (self.inverse_multiplier as u64 * diff as u64) & 0xffffffff;
+        return (prev_seed & 0xffffffff) as Seed;
     }
 
-    pub fn extract_rand(&self, seed: u32) -> u16 {
-        return (seed >> 16) as u16;
+    pub fn extract_rand(&self, seed: Seed) -> Rand {
+        return (seed >> 16) as Rand;
     }
 }
