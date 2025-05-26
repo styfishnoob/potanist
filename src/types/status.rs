0f338a66f3aaac19bd3ae9282877cc215c7389ee
styfishnoob <main.sakana@gmail.com>
2025-05-26 11:08:14 +0900

    feat: Status を実装

---
+++ src/types/status.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+use super::{iv::*, types::*};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct Status {
+    pub ivs: IVs,
+    pub gender: i16,
+    pub nature: i16,
+    pub ability: i16,
+    pub hidden_power_type: i16,
+    pub hidden_power_power: u8,
+    pub pid: Pid,
+}
