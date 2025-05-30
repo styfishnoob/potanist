44563c9433f1c0848a3bc085f41ebdf024a6b805
styfishnoob <main.sakana@gmail.com>
2025-05-30 18:17:53 +0900

    feat: SearchSeedParams からめざパに関するプロパティを削除

--- src/modules/seed_searcher.rs
+++ src/modules/seed_searcher.rs
@@ -15,8 +15,6 @@ pub struct SearchSeedParams {
     pub iv_ranges: IVRanges,
     pub nature: i16,
     pub ability: i16,
-    pub hidden_power_type: i16,
-    pub hidden_power_power: IVRange, // 30 ~ 70
     pub shiny: bool,
     pub tid: Rand,
     pub sid: Rand,
@@ -42,44 +40,71 @@ impl SeedSearcher {
         これらのパラメータから目的のシードを探索する。
         この処理で求められるシードは個体値の一つ目のシードであり、初期シードではないので注意。
     */
-    pub fn search_seed(&self, search_seed_params: SearchSeedParams) -> Vec<Seed> {
+    pub fn search_seed(&self, params: SearchSeedParams) -> Vec<Seed> {
         let mut matched_iv_1st_seed_vec: Vec<Seed> = Vec::new();
-        let (smaller_group, larger_group, forward) =
-            compare_range_group_by_complexity(&search_seed_params);
 
-        for (iv_range_1, iv_range_2, iv_range_3) in iproduct!(
+        let iv_range_group_1 = [
+            params.iv_ranges.hp.clone(),
+            params.iv_ranges.attack.clone(),
+            params.iv_ranges.defense.clone(),
+        ];
+        let iv_range_group_2 = [
+            params.iv_ranges.speed.clone(),
+            params.iv_ranges.sp_attack.clone(),
+            params.iv_ranges.sp_defense.clone(),
+        ];
+
+        let calc_complexity = |iv_ranges: &[IVRange; 3]| {
+            iv_ranges
+                .iter()
+                .fold(1, |acc, range| acc * (range.end() - range.start()))
+        };
+
+        let complexity_1 = calc_complexity(&iv_range_group_1);
+        let complexity_2 = calc_complexity(&iv_range_group_2);
+
+        let (smaller_group, larger_group, forward) = if complexity_1 < complexity_2 {
+            (iv_range_group_1, iv_range_group_2, true)
+        } else {
+            (iv_range_group_2, iv_range_group_1, false)
+        };
+
+        for (iv_1, iv_2, iv_3) in iproduct!(
             smaller_group[0].clone(),
             smaller_group[1].clone(),
             smaller_group[2].clone()
         ) {
-            let iv_group: IVGroup = [iv_range_1, iv_range_2, iv_range_3];
-            let iv_smaller_group_rand_high_msb_0 = self.rng_analyzer.iv_group_to_rand(iv_group);
-            let iv_smaller_group_rand_high_msb_1 = (1 << 15) | iv_smaller_group_rand_high_msb_0;
+            let iv_group: IVGroup = [iv_1, iv_2, iv_3];
+            let iv_rand_high_msb_0 = self.rng_analyzer.iv_group_to_rand(iv_group);
+            let iv_rand_high_msb_1 = (1 << 15) | iv_rand_high_msb_0;
+            let iv_rand_high_group = [iv_rand_high_msb_0, iv_rand_high_msb_1];
 
-            for (iv_smaller_group_rand_high, iv_smaller_group_rand_low) in iproduct!(
-                [
-                    iv_smaller_group_rand_high_msb_0,
-                    iv_smaller_group_rand_high_msb_1
-                ],
-                0..=0xffff
-            ) {
-                let (iv_1st_seed, iv_2nd_iv_group) = derive_iv_1st_seed_and_iv_2nd_iv_group(
-                    iv_smaller_group_rand_high,
-                    iv_smaller_group_rand_low,
-                    forward,
-                    &self.rng_analyzer,
-                    &self.rng_lc,
-                );
+            for (iv_rand_high, iv_rand_low) in iproduct!(iv_rand_high_group, 0..=0xffff) {
+                let (iv_1st_seed, iv_2nd_iv_group) = if forward {
+                    let iv_1st_seed = self.rng_analyzer.rands_to_seed(iv_rand_high, iv_rand_low);
+                    let iv_2nd_seed = self.rng_lc.next(iv_1st_seed);
+                    let iv_2nd_rand = self.rng_analyzer.extract_rand(iv_2nd_seed);
+                    let iv_2nd_iv_group = self.rng_analyzer.rand_to_iv_group(iv_2nd_rand);
+                    (iv_1st_seed, iv_2nd_iv_group)
+                } else {
+                    let iv_2nd_seed = self.rng_analyzer.rands_to_seed(iv_rand_high, iv_rand_low);
+                    let iv_1st_seed = self.rng_lc.prev(iv_2nd_seed);
+                    let iv_1st_rand = self.rng_analyzer.extract_rand(iv_1st_seed);
+                    let iv_2nd_iv_group = self.rng_analyzer.rand_to_iv_group(iv_1st_rand);
+                    (iv_1st_seed, iv_2nd_iv_group)
+                };
 
                 let ivs_contains_range = larger_group
                     .iter()
                     .zip(iv_2nd_iv_group.iter())
                     .all(|(range, value)| range.contains(value));
 
-                let extracted_status = self.seed_analyzer.extract_status(iv_1st_seed);
-                let check_status = check_status(&extracted_status, &search_seed_params);
+                let status = self.seed_analyzer.extract_status(iv_1st_seed);
+                let check_nature = params.nature == -1 || params.nature == status.nature;
+                let check_ability = params.ability == -1 || params.ability == status.ability;
+                let check_shiny = !params.shiny || is_shiny(params.tid, params.sid, status.pid);
 
-                if ivs_contains_range && check_status {
+                if ivs_contains_range && check_nature && check_ability && check_shiny {
                     matched_iv_1st_seed_vec.push(iv_1st_seed)
                 }
             }
@@ -98,9 +123,8 @@ impl SeedSearcher {
         /*
             time_sum が 0..=0xff な理由は、ffから値が折り返すため。
             0(0x0), 1(0x1), ... , 255(0xff), 256(0x100), 257(0x101)
-            で、time_sum は 実質 0xff されるため、256(0x100) は 0x00 になる。
-            そのため 256(0x100) は 0(0x0) と同じ値になる。
-            よって、256(0x00) = 0, 257(0x101) = 1, 258(0x102) = 2 ... と、256を折り返しにそれぞれ対応していく。
+            で、time_sum は実質 & 0xff されるため、256(0x100) は 0x00 になり、
+            256(0x00) = 0, 257(0x101) = 1, 258(0x102) = 2 ... と、256を折り返しにそれぞれ　0, 1, 2, ...　に対応していく。
             なので、0 から 255 まで回すだけでよく、time_sumを表示する際は、0x00 と 0x00 の左端に 1 を足し、256(0x100) を表示させればいい。
             ただし、time_sum の最大値は 12 * 31 + 59 + 59 の 490(0x1ea) で、それに対応する 234(0xea) 以降は実現不可能な値になるため注意。
         */
@@ -125,17 +149,6 @@ impl SeedSearcher {
                     let is_shiny = is_shiny(tid, sid, pid);
 
                     if nature_num == 3 && ability_num == 0 && 127 <= gender_num && is_shiny {
-                        println!("性格: {}", nature_num);
-                        println!("性別: {}", gender_num);
-                        println!("特性: {}", ability_num);
-                        println!("PID: {:0x}", pid);
-                        println!("消費数: {}", advances);
-                        println!("色違い: {}", is_shiny);
-                        if (1 << 8) | time_sum < 490 {
-                            println!("time_sum: {} {}", time_sum, (1 << 8) | time_sum);
-                        } else {
-                            println!("time_sum: {}", time_sum);
-                        }
                         continue 'time_sum_loop; // 外側のループへ
                     }
                 }
@@ -188,89 +201,6 @@ impl SeedSearcher {
             return None;
         }
     }
-}
-
-/*
-  (IVRangeGroup, IVRangeGroup, bool) -> (smaller_group, larget_group, forward)
-  forwardは、smaller_groupがiv_1stだった場合とiv_2ndだった場合で処理順が変わるため、こちらを返却する。
-*/
-fn compare_range_group_by_complexity(
-    search_seed_params: &SearchSeedParams,
-) -> (IVRangeGroup, IVRangeGroup, bool) {
-    let iv_range_group_1 = [
-        search_seed_params.iv_ranges.hp.clone(),
-        search_seed_params.iv_ranges.attack.clone(),
-        search_seed_params.iv_ranges.defense.clone(),
-    ];
-    let iv_range_group_2 = [
-        search_seed_params.iv_ranges.speed.clone(),
-        search_seed_params.iv_ranges.sp_attack.clone(),
-        search_seed_params.iv_ranges.sp_defense.clone(),
-    ];
-
-    let complexity_1 = iv_range_group_1
-        .iter()
-        .fold(1, |acc, range| acc * (range.end() - range.start()));
-
-    let compolexity_2 = iv_range_group_2
-        .iter()
-        .fold(1, |acc, range| acc * (range.end() - range.start()));
-
-    if complexity_1 < compolexity_2 {
-        (iv_range_group_1, iv_range_group_2, true)
-    } else {
-        (iv_range_group_2, iv_range_group_1, false)
-    }
-}
-
-fn derive_iv_1st_seed_and_iv_2nd_iv_group(
-    iv_smaller_group_rand_high: Rand,
-    iv_smaller_group_rand_low: Rand,
-    forward: bool,
-    rng_analyzer: &RandAnalyzer,
-    rng_lc: &RngLC,
-) -> (Seed, IVGroup) {
-    if forward {
-        let iv_1st_seed =
-            rng_analyzer.rands_to_seed(iv_smaller_group_rand_high, iv_smaller_group_rand_low);
-        let iv_2nd_seed = rng_lc.next(iv_1st_seed);
-        let iv_2nd_iv_group = rng_analyzer.rand_to_iv_group(rng_analyzer.extract_rand(iv_2nd_seed));
-        (iv_1st_seed, iv_2nd_iv_group)
-    } else {
-        let iv_2nd_seed =
-            rng_analyzer.rands_to_seed(iv_smaller_group_rand_high, iv_smaller_group_rand_low);
-        let iv_1st_seed = rng_lc.prev(iv_2nd_seed);
-        let iv_2nd_iv_group = rng_analyzer.rand_to_iv_group(rng_analyzer.extract_rand(iv_1st_seed));
-        (iv_1st_seed, iv_2nd_iv_group)
-    }
-}
-
-fn check_status(extracted_status: &Status, search_seed_params: &SearchSeedParams) -> bool {
-    let check_nature =
-        search_seed_params.nature == -1 || search_seed_params.nature == extracted_status.nature;
-
-    let check_ability =
-        search_seed_params.ability == -1 || search_seed_params.ability == extracted_status.ability;
-
-    let check_hidden_power_type = search_seed_params.hidden_power_type == -1
-        || search_seed_params.hidden_power_type == extracted_status.hidden_power_type;
-
-    let check_hidden_power_power = search_seed_params.hidden_power_type == -1
-        || search_seed_params
-            .hidden_power_power
-            .contains(&extracted_status.hidden_power_power);
-
-    let check_shiny = !search_seed_params.shiny || {
-        let tsid_xor = (search_seed_params.tid ^ search_seed_params.sid) as u32;
-        let pid_xor = ((extracted_status.pid >> 16) ^ (extracted_status.pid & 0xffff)) as u32;
-        (tsid_xor ^ pid_xor) <= 7
-    };
-
-    return check_nature
-        && check_ability
-        && check_hidden_power_type
-        && check_hidden_power_power
-        && check_shiny;
 }
 
 fn is_shiny(tid: Rand, sid: Rand, pid: Pid) -> bool {
